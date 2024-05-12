cf4a0eea0aab81e01163d292346225fe0559ef01
EstefaniaGrowker <vsuykyasmbp@hotmail.com>
2024-05-12 20:01:44 -0300

    feat: add responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-
 import { useRef } from "react";
 import { Inter } from "next/font/google";
 import Transcription from "@/components/Transcription";
 import transcription from "./utils";
 import audio from "../audio/Test_Call.wav";
+import logo from "../public/logoApp.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,15 +19,19 @@ export default function Home() {
 
   return (
     <main
-      className={`flex min-h-screen justify-center gap-10 p-24 ${inter.className} bg-gradient-to-b from-cyan-950 to-indigo-700`}
+      className={`flex min-h-screen flex-col p-4 md:p-24 ${inter.className} bg-gradient-to-b from-cyan-950 to-indigo-700`}
     >
-      <Transcription transcription={transcription} playFrom={playFrom} />
-      <div >
-        <audio ref={audioRef} controls>
-          <source src={audio} type="audio/mpeg" />
-          Your browser does not support the audio element.
-        </audio>
+      <h1 className="text-3xl font-bold tracking-wide text-cyan-50 self-center">Audio Salto</h1>
+      <div className="flex flex-wrap justify-center gap-10 p-4 md:p-20">
+        <div className="relative w-72">
+          <audio ref={audioRef} controls className="min-[1192px]:fixed">
+            <source src={audio} type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+        <Transcription transcription={transcription} playFrom={playFrom} />
       </div>
+
     </main>
   );
 }
